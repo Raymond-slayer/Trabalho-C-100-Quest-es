0b6dc39675703ee7ea6555a6c8dd97287268848e
Raymond-slayer <raimundo_dh@outlook.com>
2025-04-10 23:16:21 -0300

    Exercicio

---
+++ IFSC/main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    int N;
+
+    printf("Leia o numero:  ");
+    scanf("%i", &N);
+
+    if(N <=10){
+        printf("F1\n");
+    }else if(N > 10 && N <=100)
+    {
+    printf("F2");
+    }else{
+    printf("F3");
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
