dca28e05df3c011463d110073c609000fbdadff0
Raymond-slayer <raimundo_dh@outlook.com>
2025-04-10 23:04:38 -0300

    Exercico23.c

---
+++ IFSC/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    int NumA = 5;
+    int NumB = 9;
+
+
+
+    printf("A ordem dos numeros %d %d\n",NumB, NumA );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+system("pause");
+    return 0;
+}
