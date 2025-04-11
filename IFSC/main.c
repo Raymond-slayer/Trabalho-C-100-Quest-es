4df9591ad9d09fdd3f6bf3cba32f2a6bb7b10220
Raymond-slayer <raimundo_dh@outlook.com>
2025-04-10 22:55:06 -0300

    Add files via upload

---
+++ IFSC/main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a, b;
+
+
+    printf("Digite um numero: ");
+    scanf("%d", &a);
+
+    printf("Digite outro numero: ");
+    scanf("%d", &b);
+
+
+    int soma = a + b;
+
+    printf("O Resultado da soma e: %d\n ", soma);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
