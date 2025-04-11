bf40add91fed9f8dccd41985c7e543922e7538fe
Raymond-slayer <raimundo_dh@outlook.com>
2025-04-10 23:03:10 -0300

    Exercicio22.c

---
+++ IFSC/main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    float Val1;
+    float Val2;
+
+
+
+
+    printf("Digite o primeiro valor:");
+    scanf("%f", &Val1);
+
+    printf("Digite o segundo valor: ");
+    scanf("%f", &Val2);
+
+
+    float Media = (Val1 + Val2) /2;
+
+    printf("A media dos valores somados e de %.2f", Media);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
