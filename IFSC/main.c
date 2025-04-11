b65d340bec9f85eb57e98d157a45d8fbfe93f972
Raymond-slayer <raimundo_dh@outlook.com>
2025-04-10 23:10:01 -0300

    Exercicio27.c

---
+++ IFSC/main.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+   int a,b,c,d,e, digitoV, S;
+
+   a = 5;
+   b = 4;
+   c = 3;
+   d = 2;
+   e = 1;
+
+   S = 6*a + 5*b + 4*c + 3*d + 2*e;
+   digitoV = S % 7;
+
+
+
+
+
+
+   printf("Verificador %i",digitoV );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
