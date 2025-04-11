ea51d2dabf4be7d66e22cfac741ee9c3cf48059d
Raymond-slayer <raimundo_dh@outlook.com>
2025-04-10 23:24:18 -0300

    Add files via upload

---
+++ PURCS/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    float a = (-10.00) + (-20.00) + (-30.00) + (-40.00) + (50.00) ;
+
+
+
+    printf("1. Escrever um algoritmo que le 5 valores para a, um de cada vez, e conta quantos destes valores são negativos, escrevendo esta informacao. \n");
+    printf(" %.2f\n",a);
+    printf("Na primeira questao temos 4 valores negativos, sao eles: -10.00 -20.00 -30.00 -40.00");
+    return 0;
+
+}
