686a90943863eee2cddfe68e5d3dcd8a7841c2a8
Raymond-slayer <raimundo_dh@outlook.com>
2025-04-10 23:25:06 -0300

    Update and rename main.c to Exercicio1.c

--- PURCS/Exercicio1.c
+++ PURCS/Exercicio1.c
@@ -8,7 +8,7 @@ int main()
 
 
 
-    printf("1. Escrever um algoritmo que le 5 valores para a, um de cada vez, e conta quantos destes valores s„o negativos, escrevendo esta informacao. \n");
+    printf("1. Escrever um algoritmo que le 5 valores para a, um de cada vez, e conta quantos destes valores s√£o negativos, escrevendo esta informacao. \n");
     printf(" %.2f\n",a);
     printf("Na primeira questao temos 4 valores negativos, sao eles: -10.00 -20.00 -30.00 -40.00");
     return 0;
