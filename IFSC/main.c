f7b8f7aacf436ed7059eb5745355ff9867791028
Raymond-slayer <raimundo_dh@outlook.com>
2025-04-10 23:06:14 -0300

    Exercicio24.c

---
+++ IFSC/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    float Km_h = 100;
+    float M_s = 3.6;
+    float resultado = Km_h / M_s;
+
+
+    printf("a metragem por segundo e %.2f\n", resultado);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
