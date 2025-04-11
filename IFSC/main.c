a57804d804a9ae02e7b2fb8ef608308bece3aada
Raymond-slayer <raimundo_dh@outlook.com>
2025-04-10 22:59:54 -0300

    Exercicio19.c

---
+++ IFSC/main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+
+    int a = 10;
+    int quadrado = (int)pow(a,2);
+    int cubo = (int)pow(a,3);
+
+
+    printf("O numero inteiro e %i\n", a);
+    printf("O quadrado desse numero e %i\n", quadrado);
+    printf("O cubo desse numero e: %i", cubo);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
