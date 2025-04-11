19dfbd1670978ed0fade276ea09c6122d2f8116a
Raymond-slayer <raimundo_dh@outlook.com>
2025-04-10 23:15:30 -0300

    Exercicio31.c

---
+++ IFSC/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(){
+
+int a, b;
+
+
+
+printf("Digite um numero a:  ");
+scanf("%i", &a);
+
+printf("Digite um segundo número b:  ");
+scanf("%i", &b);
+
+
+if (a > b)
+{
+printf(" a e maior do que b");
+}
+else
+{
+printf("a nao e maior que b");
+}
+
+
+
+
+return 0;
+}
