07dc323320671f5052de0cc430585a46b8cb5b5b
Raymond-slayer <raimundo_dh@outlook.com>
2025-04-14 23:41:49 -0300

    Add files via upload

---
+++ IFSC/Exercicio48.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+
+// 48. Faça um algoritmo que leia 3 números inteiros distintos e escreva o menor deles.
+
+int main(){
+
+int a;
+int b;
+int c;
+
+
+
+
+printf(" Digite o numero de A: ");
+scanf("%i",&a);
+
+printf(" Digite o numero de b: ");
+scanf("%i",&b);
+
+
+printf(" Digite o numero de C: ");
+scanf("%i",&c);
+
+if (a < b && c){
+    printf("A e o menor valor");
+
+
+}else if(b < a && c){
+printf("B e o menor valor\n");
+
+}else if("c < a && b");{
+printf("C e o menor valor");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+return 0;
+}
