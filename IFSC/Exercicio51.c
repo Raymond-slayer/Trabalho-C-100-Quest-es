829d9412fd0820b892dfab94c5abe9f4b456122b
Raymond-slayer <raimundo_dh@outlook.com>
2025-04-14 23:46:46 -0300

    Add files via upload

---
+++ IFSC/Exercicio51.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+//51. Faça um algoritmo que leia dois números e mostre qual o maior dos dois .
+
+
+int main(){
+
+int a;
+int b;
+
+
+
+
+
+printf(" Digite o numero de A: ");
+scanf("%i",&a);
+
+printf(" Digite o numero de b: ");
+scanf("%i",&b);
+
+
+
+if (a > b ){
+    printf("A e o maior valor");
+
+
+}else{
+printf("B e o maior valor\n");
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+return 0;
+}
