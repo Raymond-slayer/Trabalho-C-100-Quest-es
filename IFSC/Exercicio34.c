4652dc52f56ed1e3d9931c63f6bc30cd57f20ef7
Raymond-slayer <raimundo_dh@outlook.com>
2025-05-12 22:05:00 -0300

    Add files via upload

---
+++ IFSC/Exercicio34.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//34. Construa um algoritmo que receba como entrada três valores e os imprima em ordem crescente.
+
+
+
+int main()
+{
+
+     int n = 123;
+
+     printf("valor crescente %d", n);
+printf("\n\n");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
