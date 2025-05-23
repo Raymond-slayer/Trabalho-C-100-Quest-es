d5544e20640fb47571bf17fd50ceac4109e80711
Raymond-slayer <raimundo_dh@outlook.com>
2025-05-22 23:16:48 -0300

    Add files via upload

---
+++ IFSC/Exercicio70.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+//70. Crie um algoritmo que retorne verdadeiro quando um número fornecido for par.
+
+int main()
+{
+
+    int number;
+
+
+    printf("digite um numero da sua preferência: ");
+    scanf("%d", &number);
+
+
+    printf("\n\n");
+
+
+    if(number % 2 == 0){
+
+        printf("Verdadeiro!!");
+
+    }else{
+    printf("Falso");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
