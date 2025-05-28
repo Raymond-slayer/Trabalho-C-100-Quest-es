4aaff376b19b27fb25c13c5e7410dd4ad4cf3fea
Raymond-slayer <raimundo_dh@outlook.com>
2025-05-28 00:46:59 -0300

    Update and rename Execicio74.c to Exercicio74.c

--- IFSC/Exercicio74.c
+++ IFSC/Exercicio74.c
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 #include <locale.h>
 
-/*74. O programa de uma loja de móveis mostra o seguinte menu na tela de vendas:
+/*74. O programa de uma loja de mÃ³veis mostra o seguinte menu na tela de vendas:
 1-Venda a Vista
 2-Venda a Prazo 30 dias
 3-Venda a Prazo 60 dias
 4-Venda a Prazo com 90 dias
-5-Venda com cartão de débito
-6-Venda com cartão de crédito
-Escolha a opção:*/
+5-Venda com cartÃ£o de dÃ©bito
+6-Venda com cartÃ£o de crÃ©dito
+Escolha a opÃ§Ã£o:*/
 
 int main()
 {
@@ -19,7 +19,7 @@ int main()
     int opcao;
 
 
-    printf("Digite as opções de venda \n1-Venda a Vista\n2-Venda a Prazo 30 dias\n3-Venda a Prazo 60 dias\n4-Venda a Prazo com 90 dia\n5-Venda com cartão de débito\n6-Venda com cartão de crédito");
+    printf("Digite as opÃ§Ãµes de venda \n1-Venda a Vista\n2-Venda a Prazo 30 dias\n3-Venda a Prazo 60 dias\n4-Venda a Prazo com 90 dia\n5-Venda com cartÃ£o de dÃ©bito\n6-Venda com cartÃ£o de crÃ©dito");
     printf("\n\n");
 scanf("%d", &opcao);
 
@@ -39,14 +39,14 @@ printf("\n\n");
         printf("Venda a prazo 90 dias");
         break;
          case 5:
-        printf("Venda com cartão");
+        printf("Venda com cartÃ£o");
         break;
          case 6:
-        printf("Venda com cartão de crédito");
+        printf("Venda com cartÃ£o de crÃ©dito");
         break;
 
          default:
-            printf("Opção inválida");
+            printf("OpÃ§Ã£o invÃ¡lida");
 
 
 
