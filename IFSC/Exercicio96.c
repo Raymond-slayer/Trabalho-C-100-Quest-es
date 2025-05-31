483dc6ab15f3a9b556d7529e80c145504df049f4
Raymond-slayer <raimundo_dh@outlook.com>
2025-05-31 00:33:42 -0300

    Add files via upload

---
+++ IFSC/Exercicio96.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int produto = 1;
+
+    for (int i = 1; i <= 15; i += 2) {
+        produto *= i;  // produto = produto * i
+    }
+
+    printf("O produto dos inteiros ímpares de 1 a 15 é: %d\n", produto);
+
+    return 0;
+}
