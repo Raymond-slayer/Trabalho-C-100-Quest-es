185d3426db7577fc1589c8d999afc65f022a61e5
Raymond-slayer <raimundo_dh@outlook.com>
2025-06-03 20:39:04 -0300

    Update Exercicio4.c

--- PURCS/Exercicio4.c
+++ PURCS/Exercicio4.c
@@ -1,53 +1,18 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <locale.h>
 
-int main()
-{
-setlocale(LC_ALL, "");
+int main() {
+    float alturaChico = 1.50;
+    float alturaZe = 1.10;
+    int anos = 0;
 
-    float Chico = 1.50;
-    float Cumprimento_cm = 0.02;
-    float Cumprimento_total_chico = Cumprimento_cm * 8 ;
-    float Cumprimento_total = Cumprimento_total_chico + Chico;
+    while (alturaZe <= alturaChico) {
+        alturaChico += 0.02;
+        alturaZe += 0.03;
+        anos++;
+    }
 
-    float Ze = 1.10;
-    float Cumprimento_cm2 = 0.03;
-    float Cumprimento_total_Ze = 0.03 * 19;
-    float Cumprimento_total2 = Cumprimento_total_Ze + Ze;
-
-
-
-
-    printf("Altura de Chico é de %.2f_cm\n\n",Chico);
-    printf("Chico terá o o cumprimento total de %.2f após 8 anos", Cumprimento_total);
-
-    printf("\n\n");
-
-    printf("Altura de Zé é de %.2f_cm\n",Ze);
-    printf("Chico terá o o cumprimento total de %.2f após 19 anos", Cumprimento_total2);
-
-    printf("\n\n");
-
-    printf("Será necessário 19 anos para Ze ser maior que Chico");
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    printf("SerÃ£o necessÃ¡rios %d anos para que ZÃ© seja mais alto que Chico.\n", anos);
 
     return 0;
 }
+
