54cebf0791dcb3d41e3f7119182900f86693074d
Raymond-slayer <raimundo_dh@outlook.com>
2025-06-03 20:40:09 -0300

    Add files via upload

---
+++ IFSC/Exercicio118.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    float alturaChico = 1.50;
+    float alturaZe = 1.10;
+    int anos = 0;
+
+    while (alturaZe <= alturaChico) {
+        alturaChico += 0.02;
+        alturaZe += 0.03;
+        anos++;
+    }
+
+    printf("Serão necessários %d anos para que Zé seja mais alto que Chico.\n", anos);
+
+    return 0;
+}
